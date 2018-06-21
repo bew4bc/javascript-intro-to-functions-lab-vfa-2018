700df3aeaf7eb3db2b04bf2383fc54a66fd21a11
Ben Weinberg <bweinberg1222@gmail.com>
2018-06-21 19:40:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logShout(string) {
 }
 
 function logWhisper(string) {
-  return console.log(string.toLowerCase())
+  console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
